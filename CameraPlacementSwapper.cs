5fef0827ca838caf0c86cf5bf3f856fcf53ee56a
Níkolas Escobar <40873324+NikoEscobar@users.noreply.github.com>
2018-07-10 05:47:53 -0300

    Refactoring and cleaning
    Just a refactoring and eliminations of some variables used for debugging purposes.

--- CameraPlacementSwapper.cs
+++ CameraPlacementSwapper.cs
@@ -4,80 +4,60 @@ using System.Collections;
 public class CameraPlacementSwapper : MonoBehaviour
 {
 
-    //botao momentaneo, e apropriado criar um setup ou um botao no hud para isso
-    public bool cameraSwitch1;
-    public bool cameraSwitch2;
-    public bool cameraSwitch3;
     public int cameraStyle;
 
-    private int mainCameraStyle;
-    private bool returnCameraDestrava;
-
-    public ShipInside ShipInsideScript;
+    public ShipInsideCollider ShipInsideColliderScript;
     public ShipCamFix ShipCamFixScript;
 
-    // Use this for initialization
+    private bool returnCameraTrigger = false;
+
+    private enum CameraPlacement
+    {
+        onPlayerDefault,
+        onPlayerFocus,
+        onShipPlongee}
+
+    ;
+
+    [SerializeField]
+    private CameraPlacement CameraPlace;
+
     void Start()
     {
-        cameraSwitch1 = true;
-        cameraSwitch2 = false;
         cameraStyle = 1;
-        mainCameraStyle = 1;
+        CameraPlace = CameraPlacement.onPlayerDefault;
 
     }
-	
-    // Update is called once per frame
+
     void Update()
     {
-        CameraSwap();
-        ForceCamera();
+        CameraPlacementSwap();
     }
 
-    void CameraSwap()
+    void CameraPlacementSwap()
     {
-        if (ShipInsideScript.insideShip == true || ShipCamFixScript.GoingToShip == true)
+        if (ShipInsideColliderScript.insideShip == true || ShipCamFixScript.GoingToShip == true)
         {
-            cameraSwitch3 = true;
-            cameraSwitch2 = false;
-            cameraSwitch1 = false;
             cameraStyle = 3;
-            returnCameraDestrava = true;
+            CameraPlace = CameraPlacement.onShipPlongee;
+            returnCameraTrigger = true;
+        }
+        else if (ShipInsideColliderScript.insideShip == false && ShipCamFixScript.GoingToShip == false && returnCameraTrigger == true)
+        {
+            cameraStyle = 1;
+            CameraPlace = CameraPlacement.onPlayerDefault;
+            returnCameraTrigger = false;
         }
 
         if (Input.GetButtonUp("CameraSwap") && cameraStyle == 1)
         {
-            cameraSwitch2 = true;
-            cameraSwitch1 = false;
             cameraStyle = 2;
-            mainCameraStyle = 2;
+            CameraPlace = CameraPlacement.onPlayerFocus;
         }
         else if (Input.GetButtonUp("CameraSwap") && cameraStyle == 2)
         {
-            cameraSwitch2 = false;
-            cameraSwitch1 = true;
             cameraStyle = 1;
-            mainCameraStyle = 1;
-        }
-    }
-
-    void ForceCamera()
-    {
-        if (ShipInsideScript.insideShip == false && ShipCamFixScript.GoingToShip == false)
-        {
-            if (mainCameraStyle == 1 && returnCameraDestrava == true)
-            {
-                cameraSwitch2 = false;
-                cameraSwitch1 = true;
-                cameraStyle = 1;
-                returnCameraDestrava = false;
-            }
-            else if (mainCameraStyle == 2 && returnCameraDestrava == true)
-            {
-                cameraSwitch2 = true;
-                cameraSwitch1 = false;
-                cameraStyle = 2;
-                returnCameraDestrava = false;
-            }
+            CameraPlace = CameraPlacement.onPlayerDefault;
         }
     }
 }
