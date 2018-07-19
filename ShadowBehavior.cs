63e69e550ec9f602d8e98a182edc9dba6b86dc78
Níkolas Escobar <40873324+NikoEscobar@users.noreply.github.com>
2018-07-19 01:33:16 -0300

    Refactoring
    I just make the code more readable.

--- ShadowBehavior.cs
+++ ShadowBehavior.cs
@@ -3,46 +3,67 @@ using System.Collections;
 
 public class ShadowBehavior : MonoBehaviour
 {
-
-    RaycastHit shadowHit;
-    public float offsetAlign = 0f;
-    private Transform shadow;
     public Transform shadowLocation;
 
-    private float normalizedDistance;
-    private Vector3 scaling = Vector3.zero;
-    public GameObject shadowQuad;
-    private MeshRenderer shadowQuadRend;
+    private Transform shadow;
 
-    void Start()
+    private RaycastHit ground;
+
+    private float offset = 0.01f;
+    private float normalizedDistance;
+    private float maxShadowVisibleDistance = 4f;
+
+    private MeshRenderer shadowRender;
+
+    void Awake()
     {
         shadow = transform;
-        shadowQuadRend = shadowQuad.GetComponent<MeshRenderer>();
+        shadowRender = GetComponentInChildren<MeshRenderer>();
     }
 
     void FixedUpdate()
     {
-        StayGrounded();
+        ApplyShadowBehaviour();
     }
 
-    void StayGrounded()
+    void ApplyShadowBehaviour()
     {
-        if (Physics.Raycast(shadowLocation.position, Vector3.down, out shadowHit, 4))
+        if (IsGroundInRange())
         {
-            shadowQuadRend.enabled = true;
-            shadow.position = new Vector3(shadowLocation.position.x, (shadowHit.point.y + offsetAlign), shadowLocation.position.z);
-            shadow.rotation = Quaternion.FromToRotation(Vector3.back, shadowHit.normal);
-            normalizedDistance = 1 - ((shadowHit.distance - offsetAlign) / 4);
-            shadow.localScale = new Vector3(normalizedDistance, normalizedDistance, normalizedDistance);
-
-            if (normalizedDistance < 0)
-            {
-                normalizedDistance = 0;
-            }
-
+            shadowRender.enabled = true;
+            SetShadowPositionClampedOnFloor();
+            ShrinkShadowByFloorDistance();
+            PreventShrinkGoingNegative();
         }
         else
-            shadowQuadRend.enabled = false;
+            shadowRender.enabled = false;
     }
 
+    private bool IsGroundInRange()
+    {
+        if (Physics.Raycast(shadowLocation.position, Vector3.down, out ground, maxShadowVisibleDistance))
+            return true;
+        else
+            return false;
+    }
+
+    private void SetShadowPositionClampedOnFloor()
+    {
+        shadow.position = new Vector3(shadowLocation.position.x, (ground.point.y + offset), shadowLocation.position.z);
+        shadow.rotation = Quaternion.FromToRotation(Vector3.back, ground.normal);
+    }
+
+    private void ShrinkShadowByFloorDistance()
+    {
+        normalizedDistance = 1 - ((ground.distance - offset) / maxShadowVisibleDistance);
+        shadow.localScale = new Vector3(normalizedDistance, normalizedDistance, normalizedDistance);
+    }
+
+    private void PreventShrinkGoingNegative()
+    {
+        if (normalizedDistance < 0)
+        {
+            normalizedDistance = 0;
+        }
+    }
 }
